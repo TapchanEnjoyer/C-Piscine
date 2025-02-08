a55ebf3c534d8df3a803ba598790db2d61c24f34
Daniil Prikhodko <dprikhod@c2r2s4.42prague.com>
2025-02-08 19:57:04 +0100

    repair ex07

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 12:50:04 by niel              #+#    #+#             */
-/*   Updated: 2025/02/08 15:52:44 by dprikhod         ###   ########.fr       */
+/*   Updated: 2025/02/08 19:56:02 by dprikhod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,27 @@ void	ft_rev_int_tab(int *tab, int size)
 	int	buf;
 	int	pos;
 
-	pos = 1;
-	while (pos <= (size / 2))
+	pos = 0;
+	while (pos < size / 2)
 	{
-		buf = *tab;
-		*tab = *(tab + (size - pos));
-		tab = tab + (size - pos);
-		*tab = buf;
-		tab = tab - (size - pos);
+		buf = *(tab + pos);
+		*(tab + pos) = *(tab + size - pos - 1);
+		*(tab + size - pos - 1) = buf;
 		pos++;
-		tab++;
 	}
 }
 
-/*#include <stdio.h>
+// #include <stdio.h>
 
-int	main(void)
-{
-	int	array[] = {2, 5, 6};
-	int	size = 3;
+// int	main(void)
+// {
+// 	int	array[] = {2, 5, 22, 6};
+// 	int	size = 4;
 
-	ft_rev_int_tab(array, size);
-	for (int i = 0; i < size; i++)
-	{
-		printf("%d \n", array[i]);
-	}
-	return (0);
-}
-*/
+// 	ft_rev_int_tab(array, size);
+// 	for (int i = 0; i < size; i++)
+// 	{
+// 		printf("%d \n", array[i]);
+// 	}
+// 	return (0);
+// }
