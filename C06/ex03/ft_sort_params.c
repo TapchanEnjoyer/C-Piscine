572eb9ac4d3191a244958f6d22952f102d010233
Niel <dyprih@gmail.com>
2025-02-22 01:05:43 +0100

    C06 ex03 #48

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: niel <niel@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 02:24:29 by niel              #+#    #+#             */
-/*   Updated: 2025/02/20 02:26:03 by niel             ###   ########.fr       */
+/*   Updated: 2025/02/22 01:05:09 by niel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,35 @@ int	ft_strlen(char *str)
 	return (n);
 }
 
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
 int	main(int ac, char *av[])
 {
-	int	i;
+	char	*key;
+	int		i;
+	int		j;
 
+	i = 1;
+	while (i < ac)
+	{
+		key = av[i];
+		j = i - 1;
+		while (j >= 1 && ft_strcmp(key, av[j]) < 0)
+		{
+			av[j + 1] = av[j];
+			j--;
+		}
+		av[j + 1] = key;
+		i++;
+	}
 	i = 1;
 	while (i < ac)
 	{
